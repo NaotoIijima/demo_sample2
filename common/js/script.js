246a46a9b2d5416b1d5bcd79f69c09871150729e
NaotoIijima <mendokotearchery@gmail.com>
2020-12-23 16:35:29 +0900

    Create script.js

---
+++ common/js/script.js
@@ -0,0 +1,19 @@
+/*var sample = new CrossfadeSample();
+window.addEventListener("click", function(){
+    // 音を鳴らす
+    setTimeout(sample.toggle(this), 50000);
+    //sample.toggle(this);
+    console.log("sound started!");
+}, { once: true});
+
+addEventListener('mousemove', {sample, handleEvent: direction},false);
+function direction(e) {
+    var camera = document.getElementById('camera');
+    var rotation = camera.getAttribute('rotation');
+    var position = document.getElementById('pos');
+    console.log(rotation.y); 
+    this.sample.crossfade(rotation.y,position.value);
+}
+
+*/
+    
